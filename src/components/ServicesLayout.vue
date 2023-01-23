d69d954b0256146ff77cccc5a867132d6f57723a
Ricardo Cardozo <ricardo_alexandrecardozo@hotmail.com>
2023-01-23 00:04:58 -0300

    Utilizando props e classes dinâmicas

--- src/components/ServicesLayout.vue
+++ src/components/ServicesLayout.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
     <h1 class="services-title">Services</h1>
+    <div class="services-container">
+      <CardServices class="green" title="Performance" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ducimus eum, distinctio veritatis non cum
+    consequuntur aliquam, recusandae repellendus vero vitae corporis placeat mollitia eligendi pariatur, velit
+    explicabo? Magnam, harum!" />
+      <CardServices class="yellow" title="Optimization" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ducimus eum, distinctio veritatis non cum
+    consequuntur aliquam, recusandae repellendus vero vitae corporis placeat mollitia eligendi pariatur, velit
+    explicabo? Magnam, harum!" />
+      <CardServices class="blue" title="Velocity" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ducimus eum, distinctio veritatis non cum
+    consequuntur aliquam, recusandae repellendus vero vitae corporis placeat mollitia eligendi pariatur, velit
+    explicabo? Magnam, harum!" />
+    </div>
+   
   </div>
 </template>
 
 <script>
+import CardServices from './UIElements/CardServices.vue';
 export default {
-
+  components: {
+    CardServices
+  }
 }
 </script>
 
 <style>
 .services-title {
- text-align: center;
- margin: 2em;
+  text-align: center;
+  margin: 2em;
+}
+
+.services-container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/UIElements/CardServices.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container-card-services" :class="color">
+    <h3>{{ title }}</h3>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    data: {
+      color: ['green', 'yellow', 'blue']
+    }
+  }
+}
+</script>
+
+<style>
+.container-card-services {
+  padding: 2em;
+  border: 2px solid #000;
+  border-radius: 10px;
+  width: 400px;
+  text-align: center;
+  margin: 1em 3em 3em;
+}
+
+.container-card-services p {
+  margin-top: 2em;
+
+}
+
+.green {
+  background-color: #42b883;
+}
+
+.yellow {
+  background-color: rgb(252, 218, 51);
+  color: #000;
+}
+
+.blue {
+  background-color: rgb(105, 51, 151);
+  color: #fff;
+}
+</style>
